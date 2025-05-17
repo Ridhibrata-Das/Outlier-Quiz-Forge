01364df88066f52735b07f9f9b3197ce3be267a8
Ridhibrata-Das <ridhibratadas@gmail.com>
2025-05-17 21:49:12 +0530

    Add files via upload

---
+++ _next/static/chunks/5e2a4920-91bc018debfaeebe.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[6152],{34052:function(n,t,e){e.d(t,{Tfp:function(){return o}});var r=e(56771);function o(n){return(0,r.w_)({tag:"svg",attr:{viewBox:"0 0 24 24",fill:"none",stroke:"currentColor",strokeWidth:"2",strokeLinecap:"round",strokeLinejoin:"round"},child:[{tag:"polyline",attr:{points:"9 18 15 12 9 6"}}]})(n)}}}]);
